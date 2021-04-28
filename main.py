5ae8dc4c95a6ae8e3ccc98c663f13b3fdd21ef14
JayRoth-sch <jr2fh@virginia.edu>
2021-04-28 09:49:01 -0400

    incorperating nikash's code into the main.py
    made another annotation to the dataset.
    main now takes input 'queries' a list of queries to be evaluated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from googlesearch import search
+
 NaN_count = 0
 line_len_mismatch_count = 0
 
@@ -55,33 +57,35 @@ def match_url(url, dset_dict):  # tries to find keys in the dict that match the 
     return possible_orgs
 
 
-def main(min_votes=25):
-    url_arr = ['https://www.politico.com/news/2021/04/25/house-republicans-florida-retreat-484588',
-               'https://www.nytimes.com/2021/04/25/us/Election-audit-Arizona-Republicans.html',
-               'https://www.dallasnews.com/news/politics/2021/04/26/donald-trump-backs-susan-wright-for-congress/',
-               'https://www.texastribune.org/2021/04/26/texas-congress-special-election-donald-trump-endorsement/',
-               'https://www.cnn.com/2021/04/26/politics/doug-collins-not-running-senate-georgia-governor/index.html',
-               'https://slate.com/news-and-politics/2021/04/joe-arpaio-donald-trump-pardon-history.html',
-               'https://dailyprogress.com/news/local/education/pvcc-officials-hope-for-fall-semester-that-resembles'
-               '-normal/article_0879bab0-a201-11eb-9307-bb03d1d2fa16.html#tracking-source=home-top-story-1',
-               'https://www.nytimes.com/2021/04/26/opinion/biden-early-childhood-education.html',
-               'https://www.newsweek.com/house-gop-attendance-joe-bidens-congressional-address-likely-slim-due-recess'
-               '-1586541',
-               'https://www.cnbc.com/2021/04/26/75-percent-of-stock-owners-wont-pay-bidens-likely-capital-gains-tax'
-               '-hike.html',
-               'https://abcnews.go.com/Politics/bidens-1st-100-days-promises-broken-progress/story?id=77242864',
-               'https://markets.businessinsider.com/news/stocks/stock-market-performance-president-biden-100-days'
-               '-best-75-years-2021-4-1030346645',
-               'https://apnews.com/article/politics-immigration-health-joe-biden-election-2020'
-               '-0bf5f72ba4194ba4247dcf7130af70b6',
-               'https://www.washingtonpost.com/politics/2021/04/26/joe-biden-live-updates/',
-               'https://nymag.com/intelligencer/2021/04/joe-biden-first-100-days.html']
+def query_bias(query, dset_dict, discount=False):
+    rank = 1
+    results_bias = 0
+    for result in search(query, tpe="nws", num=10, stop=10, pause=2):
+        if discount:
+            if len(match_url(result, dset_dict)) > 0:
+                results_bias += float(dset_dict[match_url(result, dset_dict)[0]]['bias_val']) / rank
+            else:
+                print("no bias for:", result)
+        else:
+            if len(match_url(result, dset_dict)) > 0:
+                results_bias += float(dset_dict[match_url(result, dset_dict)[0]]['bias_val'])
+            else:
+                print("no bias for:", result)
+        rank += 1
+    return results_bias
+
+
+def main(q_arr, min_votes=25):
+    queries_bias = []
     dset_path = 'allsides.csv'
     dset_dict = dict_from_csv(dset_path, min_votes)
     print(NaN_count)  # number of sources for which the bias is not determined
     print(line_len_mismatch_count)  # number of lines in the dataset that were processed incorrectly
-    for url in url_arr:
-        print(match_url(url, dset_dict))
+    for query in q_arr:
+        queries_bias.append(query_bias(query, dset_dict, discount=True))
+    print(queries_bias)
 
 
-main()
+queries = ["Is Obama a good president?", "Is Obama a bad president?"]
+
+main(queries)
