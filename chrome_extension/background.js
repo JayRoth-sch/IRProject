d9cfd8d2b40eff53f8a7b08279d40e377d80abf5
Nikash Sethi <nikashsethi@gmail.com>
2021-05-12 22:41:11 -0400

    demo to show suggested fair results with google search

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,6 +1,6 @@
 function resetDefaultSuggestion() {
     chrome.omnibox.setDefaultSuggestion({
-      description: "Hit enter to see Google results! Alternatively, try these more fair results..."
+      description: "Try these more fair results! Alternatively, hit enter to see Google's results"
     });
   }
   
@@ -13,27 +13,22 @@ function resetDefaultSuggestion() {
       });
     clearTimeout(timeout);
     timeout = setTimeout(function () {
-        // const url = "https://cors-anywhere.herokuapp.com/http://www.jayrothenberger.com/test/"
-        // const p = {
-        //     method: "GET",
-        //     query: text
-        // };
         x = new XMLHttpRequest();
-        x.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.jayrothenberger.com/test/');
+        x.onreadystatechange = function() {
+            if (x.readyState === 4) {
+                resetDefaultSuggestion();
+                response = JSON.parse(x.responseText);
+                suggestions = [];
+                topfive = response.fair_top_5.replace('[', '').replace(']','').replaceAll('\'', '').split(", ");
+                topfive.forEach(function(suggestion) {
+                    suggestions.push({content: suggestion, description: suggestion});
+                });
+                suggest(suggestions);
+            }
+        }
+        x.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.jayrothenberger.com/test/?query='+text);
         x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        x.onload = function() {
-            resetDefaultSuggestion();
-        };
         x.send();
-        // fetch(url, p)
-        // .then(data=>{return data.json()})
-        // .then(res=>{
-        //     resetDefaultSuggestion();
-        //     suggest([
-        //         {content: "res", description: res},
-        //         {content: "https://www.nbcnews.com/politics/donald-trump/trump-takes-victory-lap-after-cheney-s-ouster-house-republican-n1267081", description: "https://www.nbcnews.com/politics/donald-trump/trump-takes-victory-lap-after-cheney-s-ouster-house-republican-n1267081"}
-        //     ])
-        // })
     }, 1000);
     
   });
