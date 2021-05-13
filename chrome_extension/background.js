004ebb2d99cd3f4ee2f5122dd59d91d45dc219c3
Nikash Sethi <nikashsethi@gmail.com>
2021-05-12 21:41:18 -0400

    make request to jay's api (broken

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,13 +1,41 @@
 function resetDefaultSuggestion() {
     chrome.omnibox.setDefaultSuggestion({
-      description: 'Hit enter when done with your query!'
+      description: "Hit enter to see Google results! Alternatively, try these more fair results..."
     });
   }
   
   resetDefaultSuggestion();
   
+  let timeout = null;
   chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-    // Suggestion code will end up here.
+    chrome.omnibox.setDefaultSuggestion({
+        description: "Loading fair results..."
+      });
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+        // const url = "https://cors-anywhere.herokuapp.com/http://www.jayrothenberger.com/test/"
+        // const p = {
+        //     method: "GET",
+        //     query: text
+        // };
+        x = new XMLHttpRequest();
+        x.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.jayrothenberger.com/test/');
+        x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        x.onload = function() {
+            resetDefaultSuggestion();
+        };
+        x.send();
+        // fetch(url, p)
+        // .then(data=>{return data.json()})
+        // .then(res=>{
+        //     resetDefaultSuggestion();
+        //     suggest([
+        //         {content: "res", description: res},
+        //         {content: "https://www.nbcnews.com/politics/donald-trump/trump-takes-victory-lap-after-cheney-s-ouster-house-republican-n1267081", description: "https://www.nbcnews.com/politics/donald-trump/trump-takes-victory-lap-after-cheney-s-ouster-house-republican-n1267081"}
+        //     ])
+        // })
+    }, 1000);
+    
   });
   
   chrome.omnibox.onInputCancelled.addListener(function() {
@@ -21,7 +49,11 @@ function resetDefaultSuggestion() {
   }
   
   chrome.omnibox.onInputEntered.addListener(function(text) {
-    // get de-biased text
-    debiased = text;
-    navigate("https://www.google.com/search?q=" + "A DEBIASED VERSION OF " + debiased);
+    if (text.substr(0, 4) == "http") {
+        navigate(text)
+    }
+    else {
+        navigate("https://www.google.com/search?q=" + text);
+    }
+    
   });
